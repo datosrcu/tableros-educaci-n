0509ae0c96a32b79d707180dd2f3284813f92641
pablofabbroni <pablofabbroni@gmail.com>
2026-07-22 10:08:03 -0300

    fix: replace underscore attribute check with is_select filter to prevent django template syntax error

--- users/templatetags/form_filters.py
+++ users/templatetags/form_filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
+
+@register.filter(name='is_select')
+def is_select(field):
+    from django.forms.widgets import Select
+    return isinstance(field.field.widget, Select)
