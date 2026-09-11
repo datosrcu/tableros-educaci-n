3b22ab8dab0de72367346ad9fa0097c10a02e180
datosrcu <datos@riocuarto.gov.ar>
2026-09-11 11:30:46 -0300

    fix(tableros): incorporar signo $ y separadores de miles en costos

--- alumnos/templatetags/filters.py
+++ alumnos/templatetags/filters.py
@@ -4,3 +4,14 @@ register = template.Library()
 @register.filter
 def to_int(start, end):
     return range(start, end+1)
+
+@register.filter(name='separador_miles')
+def separador_miles(val):
+    if val is None or val == '':
+        return '0'
+    try:
+        clean_str = str(val).replace('$', '').strip()
+        num = int(round(float(clean_str)))
+        return f"{num:,}".replace(',', '.')
+    except (ValueError, TypeError):
+        return str(val)
